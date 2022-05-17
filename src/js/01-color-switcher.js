38e9fcb6a2b884e1e7d8cece0ef67d0fe05b61ad
ViacheslavDedov <dedoff.v@gmail.com>
2022-05-17 12:43:57 +0300

    dz 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const refs = {
+    btnStart: document.querySelector('button[data-start]'),
+    btnStop: document.querySelector('button[data-stop]'),
+    body: document.querySelector('body')
+}
 
+refs.body.classList.add('switcher');
+
+refs.btnStart.addEventListener('click', (event) => {
+    event.currentTarget.setAttribute('disabled', '');
+        timerId = setInterval (() => {
+            refs.body.style.backgroundColor = getRandomHexColor();}, 1000);
+});
+
+refs.btnStop.addEventListener('click', () => {
+    clearInterval(timerId);
+    refs.btnStart.removeAttribute('disabled', '');
+});
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
