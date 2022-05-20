5a6cdc91d6a95542da57c5a19f2a9edee0623816
ViacheslavDedov <dedoff.v@gmail.com>
2022-05-20 15:11:52 +0300

    dz 9/3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formRef = document.querySelector('.form')
+formRef.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  let {
+    elements: { delay, step, amount },
+  } = event.currentTarget;
+  delay = Number(delay.value);
+  step = Number(step.value);
+  amount = Number(amount.value);
+
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise (position, delay)
+      .then(({ position, delay }) => {
+        setTimeout(() => {
+          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, { useIcon: false });
+        }, delay);
+    })
+      .catch(({ position, delay }) => {
+        setTimeout(() => {
+          Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, { useIcon: false });
+        }, delay);
+      });
+    delay += step;
   }
+}
+
+function createPromise (position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      // Fulfill
+      resolve({ position, delay });
+    } else {
+      // Reject
+      reject({ position, delay });
+    }
+  });
 }
