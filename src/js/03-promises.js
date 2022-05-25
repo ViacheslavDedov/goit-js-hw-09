5b6358f6dd4fd0b73cfaa6404d492cea30c58cb0
ViacheslavDedov <dedoff.v@gmail.com>
2022-05-25 09:44:34 +0300

    fixed dz 9/3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -13,16 +13,16 @@ function onFormSubmit(event) {
   step = Number(step.value);
   amount = Number(amount.value);
 
-  for (let i = 1; i <= amount; i += 1) {
-    createPromise (i, delay)
-      .then(({ i, delay }) => {
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise (position, delay)
+      .then(({ position, delay }) => {
         setTimeout(() => {
-          Notify.success(`✅ Fulfilled promise ${i} in ${delay}ms`);
+          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
         }, delay);
     })
-      .catch(({ i, delay }) => {
+      .catch(({ position, delay }) => {
         setTimeout(() => {
-          Notify.failure(`❌ Rejected promise ${i} in ${delay}ms`);
+          Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
         }, delay);
       });
     delay += step;
